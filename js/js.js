9ca603d693383edc7135ae66c0fee1c8c3b33fbd
Adrianna Dziewulska <dziewulska37@gmail.com>
2022-02-04 14:11:55 +0100

    hamburger

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
-const navigationSwitcher = document.querySelector('.page__navigation--switcher--js');
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
 
 navigationSwitcher.addEventListener('click', (e) => {
-    const navigationList = document.querySelector('.page__navigation-list--js');
-    navigationList.classList.toggle('page__navigation-list--visible');
+    const navigationList = document.querySelector('.page__navigation--top-list--js');
+    navigationList.classList.toggle('page__navigation--top-list--visible');
 })
